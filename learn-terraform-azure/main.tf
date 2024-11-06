f7e2dfcc77ff6136757656eefd044fa84d17ebea
Abdulsamad <feakaustin@gmail.com>
2024-11-07 00:35:01 +0100

    create variable

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -24,10 +24,14 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+variable "resource_group_name" {
+  default = "myTFResourceGroup"
+}
+
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet" {
   name                = "myTFVnet"
   address_space       = ["10.0.0.0/16"]
   location            = "westus2"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
 }
